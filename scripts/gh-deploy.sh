0032c8ef520c6eae7bb7c4071f97932c737cb629
Petr Tripolsky <tripolskypetr@gmail.com>
2022-01-10 18:29:36 +0300

    auto-deploy

---
+++ scripts/gh-deploy.sh
@@ -0,0 +1,7 @@
+rm -rf ./temp
+git clone https://github.com/react-declarative/react-declarative.github.io.git ./temp
+cp -rlf ./dist/* ./temp
+cd temp
+git add *
+git commit -m $(date +%s)
+git push
