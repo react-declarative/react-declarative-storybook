2572e7541086e8aac7f113948ac0b80ad5783333
Petr Tripolsky <tripolskypetr@gmail.com>
2022-02-21 11:55:36 +0300

    tabs-layout

---
+++ src/stories/One/Layouts/Tabs.stories.tsx
@@ -0,0 +1,57 @@
+import { FieldType, TypedField } from 'react-declarative';
+
+import { LayoutTemplate } from '../../../components/LayoutTemplate';
+
+export const Tabs = LayoutTemplate.bind({});
+
+const fields: TypedField[] = [
+  {
+    type: FieldType.Paper,
+    innerPadding: "0",
+    child: {
+      type: FieldType.Tabs,
+      tabList: [
+        "First",
+        "Second",
+        "Third"
+      ],
+      fields: [
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Typography,
+              typoVariant: 'body1',
+              placeholder: 'First Tab',
+            },
+          ],
+        },
+        {
+          type: FieldType.Typography,
+          typoVariant: 'body1',
+          placeholder: 'Second Tab',
+        },
+        {
+          type: FieldType.Typography,
+          typoVariant: 'body1',
+          placeholder: 'Third Tab',
+        },
+      ],
+    },
+  },
+];
+
+Tabs.argTypes = {
+  fields: {
+    name: 'fields',
+    defaultValue: fields,
+    readonly: true,
+    control: {
+      type: 'object',
+    },
+  }
+};
+
+export default {
+  title: 'One/Layouts',
+};
